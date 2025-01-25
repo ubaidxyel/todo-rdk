703d7c75b027648a54f49dd4bcd07a335a1016c4
ubaidxyel <midnightmarshal525@gmail.com>
2025-01-25 15:19:41 +0500

    changes in tailwind css classes

--- rdk/src/App.jsx
+++ rdk/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div>
-      <h1 className='text-black font-bold text-3xl mt-10 text-center'>Redux Toolkit Todo List</h1>
+      <h1 className='text-white font-bold text-3xl mt-10 text-center'>Redux Toolkit Todo List</h1>
       <AddTodo />
       <Todos />
     </div>
